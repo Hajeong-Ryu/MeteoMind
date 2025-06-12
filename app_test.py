a35be1b290fe02ee26b3a09569083b3f9767d641
Yeon0124 <rlatodus9@naver.com>
2025-06-13 02:42:54 +0900

    미세먼지 정보 까지 나오게 수정

---
+++ app_test.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from get_weather_test import get_weather_by_city
+from recommender_test import generate_recommendations
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html") # index_html 불러오는 부분
+
+@app.route("/result", methods=["POST"])
+def result():
+    city = request.form["city"]
+    parsed_data = get_weather_by_city(city)
+    if not parsed_data:
+        return f"<h2>{city}에 대한 정보를 찾을 수 없습니다."
+    recommendations = generate_recommendations(parsed_data)
+    return render_template("result.html", results=recommendations, city=city)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
+
